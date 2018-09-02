b73df490aab94cefac68c3c6988fbe3294f035f1
Kweny <iamkweny@gmail.com>
2018-09-03 01:48:59 +0800

    字符串

--- src/main/java/org/lomcat/leo/aide/StringAide.java
+++ src/main/java/org/lomcat/leo/aide/StringAide.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Lomcat and/or its affiliates..
  *
- * This file is part of Leo Aide [1].
+ * This file is part of Lomcat Leo Aide [1].
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  *
- * [1] Leo Aide: http://leo.lomcat.org
+ * [1] Lomcat Leo: http://leo.lomcat.org
  */
 
 package org.lomcat.leo.aide;
